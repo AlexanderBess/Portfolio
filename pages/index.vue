29f45041c30d33b3b3b94f9288e83f64f7e18bf0
AlexanderBess <alexbessmelcev@gmail.com>
2022-03-27 20:36:49 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="container__navigation">
+      <a class="navigation__button">About me</a>
+      <a class="navigation__button">Portfolios</a>
+      <a class="navigation__button">Recommendations</a>
+      <a class="navigation__button">Interesting Facts</a>
+    </div>
+    <div class="container__action">
+      2
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
