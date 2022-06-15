692f82348d319a459fe2158e5a2dac0164440ea3
AlexanderBess <alexbessmelcev@gmail.com>
2022-06-15 14:21:20 +0700

    master/feature/added header and first block on page

---
+++ layouts/default.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="primary">
+    <div class="primary__template template">
+      <div class="template__header header">
+        <div class="header__container">
+          <a>Home</a>
+          <a>Home</a>
+          <a>Home</a>
+          <a>Home</a>
+          <a>Home</a>
+        </div>
+        <div class="header__line" />
+      </div>
+      <div class="template__content">
+        <nuxt />
+      </div>
+      <div class="template__footer">
+        Footer
+      </div>
+    </div>
+    <base-modal-container />
+    <loader-screen />
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    // fake loader
+    // this.SetLoader(true);
+    // setTimeout(() => {
+    //   this.SetLoader(false);
+    // }, 1000);
+  },
+};
+</script>
+<style lang="scss" scoped>
+.primary {
+  height: 100vh;
+  overflow-y: auto;
+  &__template {
+    height: 100%;
+    display: grid;
+    grid-template-rows: 100px 1fr auto;
+  }
+}
+
+.template {
+  height: 100%;
+  min-height: 100vh;
+  overflow: auto;
+  &__header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+  }
+}
+
+.header {
+  &__container {
+    @include container;
+    display: flex;
+    justify-content: space-between;
+    a {
+      font-family: 'Gilroy-Bold';
+      font-size: 18px;
+      line-height: 22px;
+      color: #828282;
+      cursor: pointer;
+    }
+    a:hover {
+      color: #070707;
+    }
+  }
+  &__line {
+    @include container;
+    height: 1px;
+    margin-top: 25px;
+    background-color: #828282;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="example">
-    <div class="example__content">
-      <div class="example__title">
-        Title
+  <div class="content">
+    <div class="content__head head">
+      <div class="head__text">
+        <div class="head__title">
+          Aleksandr Bessmeltsev
+        </div>
+        <div class="head__description">
+          Frontend developer<br>22 years old, Tomsk
+        </div>
       </div>
+      <img
+        class="head__image"
+        src="~assets/img/background/main_pc.png"
+      >
+    </div>
+    <div class="content__blocks">
+      123
     </div>
   </div>
 </template>
@@ -14,14 +26,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.example {
+.content {
   @include container;
-  background: #dbdfe3;
-  &__content {
-    height: 300px;
+  &__head {
+    display: grid;
+    padding-bottom: 100px;
+  }
+}
+.head {
+  &__image {
+    width: 100%;
+  }
+  &__text {
+    display: grid;
+    grid-template-columns: 30% auto;
   }
   &__title {
+    font-family: 'Gilroy-Bold';
     font-size: 60px;
+  }
+  &__description {
+    font-family: 'Gilroy-Medium';
+    font-size: 18px;
+    display: flex;
+    align-items: flex-end;
+    padding-bottom: 20px;
+    justify-content: flex-end;
   }
   @include _1199 {
     &__title {
